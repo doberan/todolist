99057e63ae4ec816dd95a5e5aa7c4a64a91d3439
doberankei <masayathegantz@gmail.com>
2016-06-29 13:17:56 +0900

    Create function is a saveText and showText

--- js/create.js
+++ js/create.js
@@ -1,1 +1,32 @@
-alert("Hello warld!");
+$(loaded);
+
+function loaded() {
+  showText();
+  // ボタンをクリックしたときに実行するイベントを設定する
+  $("#formButton").click(
+    // コールバックとしてメソッドを引数にわたす
+    function() {
+      saveText();
+      showText();
+    });
+}
+
+
+function saveText() {
+  var text = $("#formText");
+  var time = new Date();
+  localStorage.setItem(time, text.val());
+  text.val("");
+}
+
+function showText() {
+  var list = $("#list")
+  list.children().remove();
+  var key, value, html = [];
+  for(var i=0, len=localStorage.length; i<len; i++) {
+    key = localStorage.key(i);
+    value = localStorage.getItem(key);
+    html.push("<p>" + value + "</p>");
+  }
+  list.append(html.join(''));
+}
